4b7ec7014411644d709e745913c32879ef27a2ae
mustafaa <mustafali.0522@gmail.com>
2024-02-10 23:46:05 +0700

    fix

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -2,22 +2,20 @@ import MovieControls from "./MovieControls";
 
 const MovieCard = ({ movie, type }) => {
   return (
-    <a href={`/movies/detail/${movie.id}`}>
-      <div className="movie-card">
-        <div className="overlay"></div>
+    <div className="movie-card">
+      <div className="overlay"></div>
 
-        {movie.poster_path ? (
-          <img
-            src={`${import.meta.env.VITE_BASEIMGURL}${movie.poster_path}`}
-            alt={movie.title}
-          />
-        ) : (
-          <div className="filler-poster"></div>
-        )}
+      {movie.poster_path ? (
+        <img
+          src={`${import.meta.env.VITE_BASEIMGURL}${movie.poster_path}`}
+          alt={movie.title}
+        />
+      ) : (
+        <div className="filler-poster"></div>
+      )}
 
-        <MovieControls type={type} movie={movie}></MovieControls>
-      </div>
-    </a>
+      <MovieControls type={type} movie={movie}></MovieControls>
+    </div>
   );
 };
 export default MovieCard;
