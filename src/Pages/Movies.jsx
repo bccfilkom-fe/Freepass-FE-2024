0d5a7d93bdb48f0160e27f33b409bbf4aecb1874
Xxnagi <119501159+Xxnagi@users.noreply.github.com>
2024-02-11 23:22:21 +0700

    Update Movies.jsx

--- src/Pages/Movies.jsx
+++ src/Pages/Movies.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { getMovieList } from "../api-util.JS";
+// Movies.jsx
+import React, { useEffect, useState, useContext } from "react";
+import { getMovieList } from "../api-util.js";
 import Pagination from "../component/Pagination";
+import { GlobalContext } from "../context/GlobalState.jsx";
+import { Link } from "react-router-dom";
 
 const Movies = () => {
   const [moviesList, setMoviesList] = useState([]);
   const [page, setPage] = useState(1);
   const [lastPage, setLastPage] = useState(1);
+  const { addMovieToWatchlist } = useContext(GlobalContext);
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -30,19 +34,30 @@ const Movies = () => {
           </div>
           <div className="movie-grid">
             {moviesList.map((movie) => (
-              <div className="movie-card">
-                <div className="overlay"></div>
-                {movie.poster_path ? (
-                  <img
-                    src={`${import.meta.env.VITE_BASEIMGURL}${
-                      movie.poster_path
-                    }`}
-                    alt={movie.title}
-                  />
-                ) : (
-                  <div className="filler-poster"></div>
-                )}
-              </div>
+              <Link to={`/movies/detail/${movie.id}`} key={movie.id}>
+                <div className="movie-card">
+                  <div className="overlay"></div>
+
+                  {movie.poster_path ? (
+                    <img
+                      src={`${import.meta.env.VITE_BASEIMGURL}${
+                        movie.poster_path
+                      }`}
+                      alt={movie.title}
+                    />
+                  ) : (
+                    <div className="filler-poster"></div>
+                  )}
+                  <div className="inner-card-controls">
+                    <button
+                      className="ctrl-btn"
+                      onClick={() => addMovieToWatchlist(movie)}
+                    >
+                      <i className="fa-solid fa-plus"></i>
+                    </button>
+                  </div>
+                </div>
+              </Link>
             ))}
           </div>
           <Pagination
